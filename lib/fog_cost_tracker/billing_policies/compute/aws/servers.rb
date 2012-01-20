1e49ba928d57792713e4b7380c2967143867c68f
Benton Roberts <benton@bentonroberts.com>
2012-01-20 17:59:09 -0500

    Reorganize BillingPolicies

---
+++ lib/fog_cost_tracker/billing_policies/compute/aws/servers.rb
@@ -0,0 +1,14 @@
+module FogCostTracker
+  module Billing
+    module Compute
+      module AWS
+        class VolumeBillingPolicy
+          # returns the cost for a particular resource over some duration (in seconds)
+          def getCostForTime(resource, duration)
+            0
+          end
+        end
+      end
+    end
+  end
+end

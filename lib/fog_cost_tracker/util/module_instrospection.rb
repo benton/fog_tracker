5dc13229773d7d2e84390ca2eb2f654356fc8ffd
Benton Roberts <benton@bentonroberts.com>
2012-01-20 06:51:16 -0500

    add Module heirarchy introspection

---
+++ lib/fog_cost_tracker/util/module_instrospection.rb
@@ -0,0 +1,15 @@
+# Cloud Resources are only tracked if a module is defined for them like:
+#     FogCostTracker::Billing::[provider]::[service]::[resource]
+# To accomplish this, the Module heirarchy needs to be inspectable
+class Module
+  def submodules
+    constants.collect {|const_name| const_get(const_name)}.select do |const|
+      const.class == Module
+    end
+  end
+  def classes
+    constants.collect {|const_name| const_get(const_name)}.select do |const|
+      const.class == Class
+    end
+  end
+end

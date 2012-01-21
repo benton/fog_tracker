4f25056eb00d577cbe5cc228e2805338a7f8c316
Benton Roberts <benton@bentonroberts.com>
2012-01-21 11:53:13 -0500

    BillingRecord validation tests

--- spec/lib/cloud_cost_tracker/models/billing_record_spec.rb
+++ spec/lib/cloud_cost_tracker/models/billing_record_spec.rb
@@ -7,7 +7,7 @@ module CloudCostTracker
         :account        => "fake_account_ID",
         :resource_id    => "fake_resource_ID",
         :resource_type  => "fake_resource_type",
-        :start_time     => Time.now,
+        :start_time     => Time.now - CloudCostTracker::DEFAULT_POLLING_TIME,
         :stop_time      => Time.now,
         :cost_per_hour  => 0.0,
         :total_cost     => 0.0,
@@ -23,8 +23,40 @@ module CloudCostTracker
       @bill.should be_valid
     end
 
-    it "is not valid without an resource ID" do
+    it "is not valid without a provider name" do
+      @bill.provider = nil
+      @bill.should_not be_valid
+    end
+    it "is not valid without a service name" do
+      @bill.service = nil
+      @bill.should_not be_valid
+    end
+    it "is not valid without an account name" do
+      @bill.account = nil
+      @bill.should_not be_valid
+    end
+    it "is not valid without a resource ID" do
       @bill.resource_id = nil
+      @bill.should_not be_valid
+    end
+    it "is not valid without a resource type" do
+      @bill.resource_type = nil
+      @bill.should_not be_valid
+    end
+    it "is not valid without a start time" do
+      @bill.start_time = nil
+      @bill.should_not be_valid
+    end
+    it "is not valid without a stop time" do
+      @bill.stop_time = nil
+      @bill.should_not be_valid
+    end
+    it "is not valid without an hourly cost" do
+      @bill.cost_per_hour = nil
+      @bill.should_not be_valid
+    end
+    it "is not valid without a total cost" do
+      @bill.total_cost = nil
       @bill.should_not be_valid
     end
 

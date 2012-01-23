b3a81ca77312f75efca381a3bfbe7dd4669d11ee
Benton Roberts <broberts@mdsol.com>
2012-01-23 17:09:51 -0500

    Remove uneeded testing code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,5 @@
 Bundler.require # Load all gems and libs
 
-# Establish ActiveRecord connection
-db_conf_file = File.expand_path('../../config/database.yml', __FILE__)
-puts "Using DB #{db_conf_file}..."
-ActiveRecord::Base.establish_connection(YAML::load(File.open(db_conf_file)))
-
 # Require RSpec support files. Logging is configured there
 support_files = Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")]
 support_files.sort.each {|f| require f}

00d9aa9de4c8c0c7dfd79f2ed08052079ea993d3
Benton Roberts <broberts@mdsol.com>
2012-01-24 12:21:55 -0500

    Test ResourceTracker.update

--- spec/lib/fog_tracker/resource_tracker_spec.rb
+++ spec/lib/fog_tracker/resource_tracker_spec.rb
@@ -5,13 +5,21 @@ module FogTracker
     describe ResourceTracker do
 
       before(:each) do
-        @tracker = ResourceTracker.new('resource_type', mock_account_tracker)
+        @tracker = ResourceTracker.new(
+          ::FAKE_COLLECTION, mock_account_tracker
+        )
       end
 
       it "exposes a collection of Fog objects" do
         @tracker.collection.should == []
       end
 
+      describe '#update' do
+        it "refreshes its resource collection from its account" do
+          @tracker.update
+          @tracker.collection.size.should be > 0
+        end
+      end
     end
 
 end

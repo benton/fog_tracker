d62eea1b3cd325429346bcc0847c95d792b3044e
Benton Roberts <broberts@mdsol.com>
2012-01-24 12:21:32 -0500

    Send log events to STDOUT during testing at level WARN

--- spec/support/_configure_logging.rb
+++ spec/support/_configure_logging.rb
@@ -1,6 +1,6 @@
 # Setup a global Logger for all tests
 LOG_LEVEL = ::Logger::WARN
-LOG = FogTracker.default_logger
+LOG = FogTracker.default_logger(STDOUT)
 LOG.info "Logging configured in #{File.basename __FILE__}."
 LOG.level = LOG_LEVEL
 #ActiveRecord::Base.logger = LOG  # Uncomment for ActiveRecord outputs

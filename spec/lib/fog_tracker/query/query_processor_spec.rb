2216b423b52f3aeabd11c580d43d3843ec0d073b
Benton Roberts <broberts@mdsol.com>
2012-01-25 13:08:33 -0500

    Test QueryProcessor with wildcard query against prepopulated resource set

--- spec/lib/fog_tracker/query/query_processor_spec.rb
+++ spec/lib/fog_tracker/query/query_processor_spec.rb
@@ -11,13 +11,6 @@ module FogTracker
       QUERY['by Fog Provider']        = '*::*::AWS::*'
       QUERY['by Fog collection name'] = '*::*::*::servers'
 
-      before(:each) do
-        @processor = QueryProcessor.new(
-          {FAKE_ACCOUNT_NAME =>
-            AccountTracker.new(FAKE_ACCOUNT_NAME, FAKE_ACCOUNT, :logger => LOG)},
-          :logger => LOG
-        )
-      end
 
       it "should define a Query Pattern for parsing queries" do
         QueryProcessor::QUERY_PATTERN.should_not == nil
@@ -28,18 +21,29 @@ module FogTracker
         context "with no discovered Resources" do
           QUERY.each do |name, query|
             it "should return an empty Array for a query #{name}" do
-              @processor.execute(query).should == []
+              QueryProcessor.new(
+                {FAKE_ACCOUNT_NAME => mock_account_tracker}, :logger => LOG
+              ).execute(query).should == []
             end
           end
         end
 
         context "with a pre-populated, diverse set of Resources" do
+          before(:each) do
+            @processor = QueryProcessor.new(
+              {FAKE_ACCOUNT_NAME => mock_account_tracker(3)}, :logger => LOG
+            )
+          end
+
           context "when running the query matching all Resources" do
             it "should return all Resources" do
-              @processor.execute(QUERY['matching all Resources']).should == []
+              @processor.execute(QUERY['matching all Resources']).should_not == []
+              @processor.execute(QUERY['matching all Resources']).size.should == 9
             end
           end
+
         end
+
       end
 
     end
--- spec/support/_mocks.rb
+++ spec/support/_mocks.rb
@@ -33,14 +33,51 @@ module FogTracker
   end
 end
 
-def mock_account_tracker
+# Some Mock Fog Resource and Collection Classes
+module Fog
+  module FakeService
+    module FakeProvider
+      class FakeCollectionType1 ; end
+      class FakeResourceType1
+        def collection ; FakeCollectionType1.new end
+      end
+      class FakeCollectionType2 ; end
+      class FakeResourceType2
+        def collection ; FakeCollectionType2.new end
+      end
+      class FakeCollectionType3 ; end
+      class FakeResourceType3
+        def collection ; FakeCollectionType3.new end
+      end
+    end
+  end
+end
+
+def mock_account_tracker(number_of_resources_per_collection = 0)
   fake_account_tracker = double('mock_account_tracker')
   fake_account_tracker.stub(:account).and_return(Hash.new)
   fake_account_tracker.stub(:name).and_return("fake account tracker")
   fake_account_tracker.stub(:log).and_return(LOG)
   fake_account_tracker.stub(:connection).and_return(mock_fog_connection)
+  fake_account_tracker.stub(:resource_trackers).and_return(
+    [
+      mock_resource_tracker(
+        Fog::FakeService::FakeProvider::FakeResourceType1,
+        number_of_resources_per_collection
+      ),
+      mock_resource_tracker(
+        Fog::FakeService::FakeProvider::FakeResourceType2,
+        number_of_resources_per_collection
+      ),
+      mock_resource_tracker(
+        Fog::FakeService::FakeProvider::FakeResourceType3,
+        number_of_resources_per_collection
+      ),
+    ]
+  )
   fake_account_tracker
 end
+
 def mock_fog_connection
   fake_fog_connection = double('mock_fog_connection')
   fake_fog_connection.stub(FogTracker::FAKE_COLLECTION).and_return([ 1, 2, 3 ])
@@ -50,5 +87,24 @@ end
 def mock_resource_tracker
   fake_resource_tracker = double('mock_resource_tracker')
   fake_resource_tracker.stub(:update)
+  fake_resource_tracker
+end
+
+def mock_fog_resource(resource_class)
+  fake_resource = resource_class.new
+  fake_collection_class =
+  fake_resource.stub(:collection).and_return(
+    collection_class.new
+  )
+  fake_resource
+end
+
+def mock_resource_tracker(resource_class, number_of_resources = 0)
+  fake_resource_tracker = double("mock_resource_tracker")
+  resources = Array.new
+  number_of_resources.times do
+    resources << resource_class.new
+  end
+  fake_resource_tracker.stub(:collection).and_return(resources)
   fake_resource_tracker
 end

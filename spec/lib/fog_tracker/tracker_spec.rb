1b26af6c2b11740f24a35e263eab92a065c43979
Benton Roberts <broberts@mdsol.com>
2012-01-26 13:38:16 -0500

    Fix Tracker callback specs

--- spec/lib/fog_tracker/tracker_spec.rb
+++ spec/lib/fog_tracker/tracker_spec.rb
@@ -34,42 +34,28 @@ module FogTracker
       end
     end
 
-    # A class for testing the Tracker's callbacks
-    class CallbackReceiver
-      def resource_callback(resource)
-        LOG.info "Found #{resource.class} #{resource.identity}"
-      end
-      def account_callback(account_name)
-        LOG.info "Updated account #{account_name}"
-      end
-    end
-
     describe '#query' do
       it "invokes any passed code block once per resulting Resource" do
-        pending "debugging of RSpec should_receive Expectation issue"
-        receiver = CallbackReceiver.new
+        receiver = double "resrouce callback object"
+        receiver.stub(:callback)
         @tracker.start
         sleep THREAD_STARTUP_DELAY
-        @tracker.query('*::*::*::*') {|r| receiver.resource_callback(r)}
-        # TODO: debug this RSpec testing problem
-        # At this point the callbacks have occurred - so...
-        receiver.should_receive(:resource_callback) # why doesn't this work?
+        res_count = @tracker.query('*::*::*::*').count
+        receiver.should_receive(:callback).exactly(res_count).times
+        @tracker.query('*::*::*::*') {|r| receiver.callback(r)}
       end
     end
 
     describe '#start' do
       it "invokes the Tracker's callback Proc when an account is updated" do
-        pending "debugging of RSpec should_receive Expectation issue"
-        receiver = CallbackReceiver.new
-        callback = Proc.new do |account_name|
-          receiver.account_callback(account_name)
-        end
-        tracker = Tracker.new(ACCOUNTS, :logger => LOG, :callback => callback)
+        receiver = double "account callback object"
+        receiver.stub(:callback)
+        tracker = Tracker.new(ACCOUNTS, :logger => LOG,
+          :callback => Proc.new {|account_name| receiver.callback(account_name)}
+        )
+        receiver.should_receive(:callback).exactly(ACCOUNTS.size).times
         tracker.start
         sleep THREAD_STARTUP_DELAY
-        # TODO: debug this RSpec testing problem
-        # At this point the callbacks have occurred - so...
-        receiver.should_receive(:account_callback) # why doesn't this work?
       end
     end
 
